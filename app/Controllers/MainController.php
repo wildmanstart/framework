0f0ff6884d0b64c915af5ef3ddf4f0ce658b7404
Persiphone <ivan.ivanov.789423@gmail.com>
2021-07-18 13:39:45 +0300

    Init project

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class MainController {
+
+    public function index() {
+        echo "Index 1";
+    }
+
+    public function index2() {
+        echo "Index 2";
+    }
+}
---
+++ config/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "index/test1" => "MainController@index",
+    "index/test2" => "MainController@index2"
+];
